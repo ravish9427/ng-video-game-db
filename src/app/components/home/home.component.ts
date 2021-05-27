f01b0a9bb82b55566f17f0bddc4df8e742455ca4
ravish9427 <ravish9427@gmail.com>
2021-05-27 13:01:59 -0700

    api call working with basic visualization.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from 'src/app/models';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +9,30 @@ import { Game } from 'src/app/models';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public sort:string;
+  public sort: string;
   public games: Array<Game>;
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacritic', params['game-search']);
+      } else {
+        this.searchGames('metacritic');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string) {
+    this.httpService
+      .getGameList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+      });
   }
 
 }
--- src/app/interceptors/http-headers-interceptor.ts
+++ src/app/interceptors/http-headers-interceptor.ts
@@ -1,22 +1,22 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable} from "rxjs";
+import { Observable } from "rxjs";
 
 @Injectable()
-export class HttpHeadersInterceptor implements HttpInterceptor{
-    constructor() {}
+export class HttpHeadersInterceptor implements HttpInterceptor {
+    constructor() { }
 
     intercept(
         req: HttpRequest<any>,
         next: HttpHandler
-    ): Observable<HttpEvent<any>>{
+    ): Observable<HttpEvent<any>> {
         req = req.clone({
-            setHeaders:{
-                'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
-                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
-            },
-            setParams:{
-                key: 'e40e743af2c94b0c916a8aa618fb4473',
+            // setHeaders: {
+            //     'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+            //     'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+            // },
+            setParams: {
+                key: 'a1d7903b774e4c4c988f7382a506d464',
             }
         });
         return next.handle(req);
